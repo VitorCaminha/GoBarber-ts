19462f8ef85fc56d06d64c222983b61375f41e06
Vitor Caminha <vitorcaminha@gmail.com>
2021-02-03 01:31:06 -0300

    Change appointments list order and reset password link

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -64,6 +64,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         ),
       },
       relations: ['user'],
+      order: { date: 'ASC' },
     });
 
     return appointments;
